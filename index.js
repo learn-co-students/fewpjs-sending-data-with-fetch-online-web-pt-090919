2ad5a06f459d64ac3c65dbed8bacc4aa5358bb43
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-04 03:44:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let formData = {
+  name: "Byron",
+  email: "Poodle"
+};
+
+function submitData(formData){
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type":
+      "application/json", "Accept": "application/json"
+    },
+    body: JSON.stringify(Object.keys(formData))
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(object){
+      console.log(object)
+    })
+    .catch(function(error){
+      alert("Bad error!");
+      console.log(error.message)
+    })
+  })
+}
